687600afaa7365cfc256f4bcfd6dcca68d0a4df3
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-09-26 18:36:51 -0400

    schemas terminado

---
+++ backend/src/models/Schedule.ts
@@ -0,0 +1,10 @@
+import { sqliteTable, integer, text } from 'drizzle-orm/sqlite-core'
+import { subject } from './Subject'
+
+export const schedule = sqliteTable('schedule',{
+    scheduleId: integer("schedule_id").primaryKey({ autoIncrement: true }),
+    startTime: text("start_time"),
+    endTime: text("end_time"),
+    date: text("password"),
+    subjectId: integer("subject_id").notNull().references( () => subject.subjectId)
+})
