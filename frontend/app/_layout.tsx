56bd81716ae5e5a0f719a5bf4c2d1b539d8c8eee
Isac Poly Bolivar Puma <119465061+polycyllo@users.noreply.github.com>
2025-10-03 16:27:55 -0400

    un seed de studiante id 1

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,12 +3,14 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
 import { ensureSchema } from '../src/db/init';
-
+import { db } from '../src/db/db';
+import { student } from '../src/db/schemas/Student';
+import { eq } from 'drizzle-orm';
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
@@ -21,7 +23,6 @@ export const unstable_settings = {
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
-
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -34,9 +35,36 @@ export default function RootLayout() {
 
   useEffect(() => {
     if (!loaded) return;
-    ensureSchema();
-    SplashScreen.hideAsync();
+    //Para mockear un studiante
+    (async () => {
+ 
+      ensureSchema();
+
+    
+      const pepitoEmail = 'pepito@gmail.com';
+      const existing = await db
+        .select()
+        .from(student)
+        .where(eq(student.email, pepitoEmail))
+        .limit(1);
+
+      if (existing.length === 0) {
+        await db.insert(student).values({
+          name: 'Pepito',
+          email: pepitoEmail,
+          password: '123456',
+        });
+         console.log('✅ Pepito creado');
+      } else {
+         console.log('ℹ️ Pepito ya existía, no se vuelve a crear');
+      }
+          const result = await db.select().from(student);
+    console.log("📂 Students en DB:", result);
+      // 3) Cierra splash
+      await SplashScreen.hideAsync();
+    })();
   }, [loaded]);
+
 
   if (!loaded) return null;
 
